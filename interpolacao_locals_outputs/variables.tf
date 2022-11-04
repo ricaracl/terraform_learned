6eb7b282acc5034c0d3ce1a9060ecc4b48ae379a
ricaracl1 <ricaracl@gmail.com>
2022-11-04 15:52:37 -0300

    terraform interpolacao

---
+++ interpolacao_locals_outputs/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  type        = string
+  description = ""
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  type        = string
+  description = ""
+  default     = "dev"
+}
